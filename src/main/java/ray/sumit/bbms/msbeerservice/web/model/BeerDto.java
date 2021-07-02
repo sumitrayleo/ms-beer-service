0aca6269ba55af9d2bccaff835d897d20240f112
Sumit Ray <sumitrayleo@gmail.com>
2021-07-03 01:14:42 +0530

    Jackson JsonCreator (113)

--- src/main/java/ray/sumit/bbms/msbeerservice/web/model/BeerDto.java
+++ src/main/java/ray/sumit/bbms/msbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package ray.sumit.bbms.msbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,9 +26,11 @@ public class BeerDto {
     @Null
     private Integer version;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH.mm.ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime createdDate;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH.mm.ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime lastModifiedDate;
 
@@ -41,8 +44,10 @@ public class BeerDto {
     @Positive
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @NotNull
     @Positive
     private BigDecimal price;
+
     private Integer quantityOnHand;
 }
